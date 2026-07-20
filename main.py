874e492769dcb977e8df0ee81cffba39a436b175
Garhapatya <116285152+Garhapatya@users.noreply.github.com>
2026-07-20 19:01:41 +0800

    资产追加

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
 import os
+from dataclasses import dataclass
 
 @register("egni_core", "Garhapatya", "支持与提供qq机器人Egni-个性化服务的核心插件", "1.0.0")
-class MyPlugin(Star):
+class EgniCore(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
-        template_path = os.path.join(
-            os.path.dirname(__file__), "assets", "tcgame", "parchement_ui.j2"
-        )
-        with open(template_path, "r", encoding="utf-8") as f:
-            self.tcgame_template = f.read()
+        """可选择实现异步的插件初始化方法，当插件被加载/启用时会调用。"""
 
 
     async def terminate(self):
@@ -31,12 +28,3 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
-
-
-
-    
-    async def custom_t2i_tmpl(self, event: AstrMessageEvent):
-        options = {} # 可选择传入渲染选项。
-        
-        url = await self.html_render(self.tcgame_template, {"items": ["吃饭", "睡觉"]}, options=options) # 第二个参数是 Jinja2 的渲染数据
-        yield event.image_result(url)
