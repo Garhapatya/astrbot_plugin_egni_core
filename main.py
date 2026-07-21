052e4c516cdac0cd472fbc2677bbb0f3b3e57bfb
Garhapatya <116285152+Garhapatya@users.noreply.github.com>
2026-07-21 11:55:18 +0800

    repeat功能

--- main.py
+++ main.py
@@ -1,30 +1,85 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.api import AstrBotConfig
 from astrbot.api import logger
 
 import os
-from dataclasses import dataclass
+import random
+from dataclasses import dataclass, field, asdict
+from typing import Any
+
+
+@dataclass
+class PluginConfig:
+    @dataclass
+    class Repeat:
+        enable: bool = True
+        probability: float = 0.4
+        blacklist: list[Any] = field(default_factory=list)
+
+        def __post_init__(self):
+            if not (0 <= self.probability <= 1):
+                self.probability = 0.4
+        
+
+    repeat: Repeat = field(default_factory=Repeat)
+
 
 @register("egni_core", "Garhapatya", "支持与提供qq机器人Egni-个性化服务的核心插件", "1.0.0")
 class EgniCore(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
+        self.setting = PluginConfig(
+            repeat=PluginConfig.Repeat(**self.config.get("repeat", {})),
+        )
+
+
 
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当插件被加载/启用时会调用。"""
+        self.repeat_blacklist = await self.get_kv_data("repeat_blacklist", []) 
+
+
+    async def save_config(self):
+        self.config.update(asdict(self.setting))
+        await self.config.save_config_async()
 
 
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        await self.save_config()
 
 
-    
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
+    async def repeat(self, event: AstrMessageEvent):
+        """自动跟风复读群友消息"""
+
+        if not self.setting.repeat.enable or event.get_group_id() in self.setting.repeat.blacklist: return    
+
+        previous_message = None
+        count = 0
+        while True:
+            message = event.get_message_outline()
+            if "[转发消息]" in message or previous_message != message:
+                count = 0
+                previous_message = message
+            elif count == -1: 
+                pass
+            else:
+                count += 1
+            
+                
+            
+            if random.random() < count * self.setting.repeat.probability:
+                event.get_messages()
+                yield
+                count = -1  
+
+            if not self.setting.repeat.enable or event.get_group_id() in self.setting.repeat.blacklist: return  
+
+
+            
+        
+
+
 
