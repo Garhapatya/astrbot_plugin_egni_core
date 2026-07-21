96257a61c36dbf07e09093497ac38e58e02b86c6
Garhapatya <116285152+Garhapatya@users.noreply.github.com>
2026-07-21 14:27:00 +0800

    repeat功能修复

--- main.py
+++ main.py
@@ -5,80 +5,49 @@ from astrbot.api import logger
 
 import os
 import random
-from dataclasses import dataclass, field, asdict
 from typing import Any
-
-
-@dataclass
-class PluginConfig:
-    @dataclass
-    class Repeat:
-        enable: bool = True
-        probability: float = 0.4
-        blacklist: list[Any] = field(default_factory=list)
-
-        def __post_init__(self):
-            if not (0 <= self.probability <= 1):
-                self.probability = 0.4
-        
-
-    repeat: Repeat = field(default_factory=Repeat)
 
 
 @register("egni_core", "Garhapatya", "支持与提供qq机器人Egni-个性化服务的核心插件", "1.0.0")
 class EgniCore(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
-        self.config = config
-        self.setting = PluginConfig(
-            repeat=PluginConfig.Repeat(**self.config.get("repeat", {})),
-        )
+        self.config: Any = config
 
-
+    repeat_memory = {}
 
     async def initialize(self):
         self.repeat_blacklist = await self.get_kv_data("repeat_blacklist", []) 
 
 
-    async def save_config(self):
-        self.config.update(asdict(self.setting))
-        await self.config.save_config_async()
-
-
     async def terminate(self):
-        await self.save_config()
-
+        pass
 
 
     @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
     async def repeat(self, event: AstrMessageEvent):
         """自动跟风复读群友消息"""
 
-        if not self.setting.repeat.enable or event.get_group_id() in self.setting.repeat.blacklist: return    
+        if not self.config.repeat.enable or event.get_group_id() in self.config.repeat.blacklist: return    
 
-        previous_message = None
-        count = 0
-        while True:
-            message = event.get_message_outline()
-            if "[转发消息]" in message or previous_message != message:
-                count = 0
-                previous_message = message
-            elif count == -1: 
-                pass
-            else:
-                count += 1
-            
-                
-            
-            if random.random() < count * self.setting.repeat.probability:
-                event.get_messages()
-                yield
-                count = -1  
-            else :
-                yield
+        message = event.get_message_outline()
+        group_id = event.get_group_id()
 
+        if (
+            group_id not in self.repeat_memory
+            or "[转发消息]" in message
+            or self.repeat_memory[group_id]["previous_message"] != message
+        ):
+            self.repeat_memory[group_id] = {"count": 0, "previous_message": message}
+        elif self.repeat_memory[group_id]["count"] == -1:
+            pass
+        else:
+            self.repeat_memory[group_id]["count"] += 1
 
-            if not self.setting.repeat.enable or event.get_group_id() in self.setting.repeat.blacklist: return  
+        if random.random() < self.repeat_memory[group_id]["count"] * self.config.repeat.probability:
+            yield event.chain_result(event.get_messages())
+            self.repeat_memory[group_id]["count"] = -1
+ 
 
 
             
